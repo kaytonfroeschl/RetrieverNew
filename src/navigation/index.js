e88b89dfabd5bf27445abe95f9529a74ee54317b
backtothefuchar <wincharans@gmail.com>
2022-03-17 22:52:54 -0500

    add comments

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,13 +1,13 @@
 /* eslint-disable prettier/prettier */
-// don't know why this is the index page lol I meant to separate it into index.js and navigation.js
-// but it works so I'm not complaining
+// Charz: don't know why this is the index page lol I meant to separate it into index.js and navigation.js
+// Charz: but it works so I'm not complaining
 import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-// I'm importing all the screens here so that they can be used in the navigation
-// If we build more screens, they need to be imported here, and also below wehre we organize them in order of access
+// Charz: I'm importing all the screens here so that they can be used in the navigation
+// Charz: If we build more screens, they need to be imported here, and also below where we organize them in order of access
 import SignInScreen from '../screens/SignInScreen';
 import SignUpScreen from '../screens/SignUpScreen';
 import EmailConfirmationScreen from '../screens/EmailConfirmationScreen/EmailConfirmationScreen';
@@ -15,8 +15,8 @@ import ResetPasswordScreen from '../screens/ResetPasswordScreen';
 import ConfirmResetPasswordScreen from '../screens/ConfirmResetPasswordScreen';
 import HomeScreen from '../screens/HomeScreen';
 
-// this bit of code I had to look up to get the theme to work
-// essentially just defined the background color of the app to be that signature dark blue color
+// Charz: this bit of code I had to look up to get the theme to work
+// Charz: essentially just defined the background color of the app to be that signature dark blue color
 const MyTheme = {
     ...DefaultTheme,
     colors: {
@@ -27,8 +27,10 @@ const MyTheme = {
 
 const Stack = createNativeStackNavigator();
 
-// this is the navigation component that we will use to navigate between screens
-// wrapping the entire thing in the NavigationContainer component so that we can use the navigation component
+// Charz: this is the navigation component that we will use to navigate between screens
+// Charz: wrapping the entire thing in the NavigationContainer component so that we can use the navigation component
+// Charz: we order them in the order of access (weird hierarchy but it works)
+// Charz: important to always have first page as SignInScreen (for now at least)
 const Navigation = () => {
     return (
         <NavigationContainer theme={MyTheme} >
@@ -45,5 +47,5 @@ const Navigation = () => {
     );
 };
 
-//we can call this screen in different .js files by importing "Navigation"
+//Charz: we can call this screen in different .js files by importing "Navigation"
 export default Navigation;
