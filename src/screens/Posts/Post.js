71875332f1fb175aecaa06e9c7312b89c27dcc8e
kaytonfroeschl <kfroeschl@gmail.com>
2022-03-29 11:35:21 -0500

    add

--- src/screens/Posts/Post.js
+++ src/screens/Posts/Post.js
@@ -10,7 +10,7 @@ import Parse from 'parse/react-native.js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 Parse.initialize('XwnlQIY0f0GyOzt5DftAZEYLOy9YZmT26ZIktF94', 'L4fRRElgmLuKvanPenznzblgXwqDJGtxIKG0dB8j');
 Parse.serverURL = 'https://parseapi.back4app.com/';
-
+te
 
 // building the home screen
 const Posts = () => {
